f39f1e4d00bc85ffe5502e40e8d920069e75ca28
panosjapan7 <panosjapan@gmail.com>
2022-03-31 14:24:41 +0200

    Add todos to array

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,25 +1,31 @@
 import React, { useState } from 'react'
 
 export default function Todos() {
-    const [todos, setTodos] = useState([
-        {
-            id: 1,
-            text: "what is up dawg",
-        }
-    ]);
-
+    const [todos, setTodos] = useState([{text: "what is up dawg",}]);
+    const [todoText, setTodoText] = useState("");
+    const addTodo = (e) => {
+        e.preventDefault();
+        setTodos([...todos, {text: todoText}])
+        setTodoText("");
+    }
 
     return (
     <div>
-        {todos.map((todo) => (
-            <div key={todo.id} >
+        {todos.map((todo, index) => (
+            <div key={index} >
                 <input type="checkbox" />
                 <label>{todo.text}</label>
             </div>
         ))}
         <br />
-        <input type="text"></input>
-        <button>Add</button>
+        <form onSubmit={addTodo}>
+            <input 
+                value={todoText}
+                onChange={(e) => setTodoText(e.target.value)}
+                type="text">    
+            </input>
+            <button type="submit">Add</button>
+        </form>
     </div>
     )
 }
