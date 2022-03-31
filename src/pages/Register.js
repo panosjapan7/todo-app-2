9f1ce448577b80c05ea4e6005ca7efa722053213
panosjapan7 <panosjapan@gmail.com>
2022-03-31 13:10:27 +0200

    show error message if username exist

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,9 +1,20 @@
 import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
+
+const handleErrors = async (response) => {
+    if(!response.ok){
+        const {message} = await response.json();
+        throw Error(message);
+    }
+    return response.json();
+}
 
 export function Register() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const navigate = useNavigate();
+
     const register = (e) => {
         e.preventDefault();
         fetch(`http://localhost:4000/register`, {
@@ -16,11 +27,19 @@ export function Register() {
                 password,
             })
         })
+        .then(handleErrors)
+        .then(() => {
+            navigate("/");
+        })
+        .catch((error) => {
+            setError(error.message);
+        })
     };
 
   return (
     <div>
         <h1>Register</h1>
+        {error && <span style={{ color: "red" }}>{error}</span>}
         <form onSubmit={register} >
             <input 
                 placeholder="username"
