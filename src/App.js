e8c8218f781a78155a2009e03f5a941ee87b4078
panosjapan7 <panosjapan@gmail.com>
2022-03-31 09:06:55 +0200

    created Welcome page,route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import Register from './pages/Register';
+import Welcome from './pages/Welcome';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        {/* <Route path="/" element={<Welcome />} />  */}
+        <Route path="/" element={<Welcome />} /> 
         <Route path="/register" element={<Register />} /> 
       </Routes>
     </div>
---
+++ src/pages/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+export function Welcome() {
+  return (
+    <div>
+        <h1>Welcome</h1>
+    </div>
+  )
+}
+
+export default Welcome
