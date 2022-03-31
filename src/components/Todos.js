75a94681c9ccd2cc03c1aba6be274ce55d7b9a72
panosjapan7 <panosjapan@gmail.com>
2022-03-31 14:11:45 +0200

    input, label for todos

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function Todos
-() {
+export default function Todos() {
+    const [todos, setTodos] = useState([]);
+
+
     return (
     <div>
-        <input id="checkbox1" type="checkbox" />
-        <label>what is up</label>
+        {todos.map(todo => {
+            <div>
+                <input id="checkbox1" type="checkbox" />
+                <label>what is up</label>
+            </div>
+        })}
+        <br />
+        <input type="text"></input>
+        <button>Add</button>
     </div>
     )
 }
