bcfbc9755c36dfe1e1a7f13cdbdf8dad1195acdd
panosjapan7 <panosjapan@gmail.com>
2022-03-31 13:33:58 +0200

    added link to Login page

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -9,6 +9,8 @@ export function Welcome() {
     <div>
         <h1>Welcome {credentials && credentials.username}</h1>
         {!credentials && <Link to="/register">Register</Link>}
+        < br/>
+        {!credentials && <Link to="/login">Login</Link>}
     </div>
     )
 }
