c421a2a2debec8c72f86aac71cd5f791145128e0
panosjapan7 <panosjapan@gmail.com>
2022-03-31 10:02:10 +0200

    created user schema, check if user already exists

--- server.js
+++ server.js
@@ -6,14 +6,29 @@ const PORT = 4000;
 
 mongoose.connect("mongodb://localhost/todo-app-2");
 
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+})
+
+const User = mongoose.model("User", userSchema)
+
 app.use(cors());
 app.use(express.json());
 
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
     const { username, password} = req.body;
+    const user = await User.findOne({ username }); //Checks if there is a username in db that's the same as the username passed in req
+    if(user){
+        res.status(500);
+        res.json({
+            message: "Username already exists"
+        });
+        return;
+    }
+    await User.create({ username, password });
     res.json({
-        username,
-        password,
+        message: "Success",
     });
 })
 
