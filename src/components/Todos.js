8505c28b8bf8baf32c1e9c1af87464a51ca5cd1e
panosjapan7 <panosjapan@gmail.com>
2022-03-31 14:55:58 +0200

    persist() sends added task to backend

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,16 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState, useContext, useEffect } from 'react';
+import { CredentialsContext } from '../App';
 
 export default function Todos() {
-    const [todos, setTodos] = useState([{text: "what is up dawg",}]);
+    const [todos, setTodos] = useState([]);
     const [todoText, setTodoText] = useState("");
+    const [credentials, setCredentials] = useContext(CredentialsContext);
+
+    
+
+    const persist = (newTodos) => {
+        fetch(`http://localhost:4000/todos`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Basic ${credentials.username}:${credentials.password}`,
+            },
+            body: JSON.stringify(newTodos) //we're passing the todos
+        })
+        .then(() => {})
+        
+    }
+
     const addTodo = (e) => {
         e.preventDefault();
         if(!todoText) return; // Prevents the user from creating a blank todo
 
         const newTodo = { checked: false, text: todoText}
-        setTodos([...todos, newTodo])
+        const newTodos = [...todos, newTodo];
+        setTodos(newTodos)
         
         setTodoText(""); // clears the text if the input that adds new todo
+        
+        persist(newTodos);
     }
 
     const toggleTodo = (index) => {
