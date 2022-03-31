80b20bca5c3f558a99585ff83efb2dc3207bc43f
panosjapan7 <panosjapan@gmail.com>
2022-03-31 09:46:33 +0200

    connection to local MongoDB, install mongoose

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = 4000;
+
+mongoose.connect("mongodb://localhost/todo-app-2");
 
 app.use(cors());
 app.use(express.json());
@@ -14,6 +17,14 @@ app.post("/register", (req, res) => {
     });
 })
 
-app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
-})
+// app.listen(PORT, () => {
+
+// })
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  app.listen(PORT, () => {
+        console.log(`Connected to MongoDB local db. Server started on port ${PORT}`);
+  });
+});
