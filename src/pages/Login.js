6fc8ffc20e2a73368e6fe58d1266ac6fb1169664
panosjapan7 <panosjapan@gmail.com>
2022-03-31 13:41:31 +0200

    created Login page with form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,67 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { CredentialsContext } from '../App'
+
+const handleErrors = async (response) => {
+    if(!response.ok){
+        const {message} = await response.json();
+        throw Error(message);
+    }
+    return response.json();
+}
 
 export default function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const navigate = useNavigate();
+    const [, setCredentials] = useContext(CredentialsContext);
+
+    const login = (e) => {
+        e.preventDefault();
+        fetch(`http://localhost:4000/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username,
+                password,
+            })
+        })
+        .then(handleErrors)
+        .then(() => {
+            setCredentials({
+                username,
+                password,
+            })
+            navigate("/");
+        })
+        .catch((error) => {
+            setError(error.message);
+        })
+    };
+
   return (
-    <div>Login</div>
+    <div>
+        <h1>Login</h1>
+        {error && <span style={{ color: "red" }}>{error}</span>}
+        <form onSubmit={login} >
+            <input 
+                placeholder="username"
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <br/>
+            <input 
+                placeholder="password"
+                onChange={(e) => setPassword(e.target.value)}
+                type="password"
+            />
+            <br/>
+            <button type="submit">Login</button>
+        </form>
+    </div>
   )
 }
+
+// export default Login
