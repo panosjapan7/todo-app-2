5a4c97c348b4167b17820d2813e6014373a4c258
panosjapan7 <panosjapan@gmail.com>
2022-04-01 13:33:24 +0200

    removed fake credentials

--- src/App.js
+++ src/App.js
@@ -8,10 +8,7 @@ import Login from './pages/Login';
 export const CredentialsContext = React.createContext();
 
 function App() {
-  const credentialsState = useState({
-    username: "test", // Temporary credentials - to be removed
-    password:  "123",
-  });
+  const credentialsState = useState(null);
   return (
     <div className="App">
       <CredentialsContext.Provider value={credentialsState}>
