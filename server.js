4a2436400c66b6e1dfda0a4c417ecb82d011e12a
panosjapan7 <panosjapan@gmail.com>
2022-04-01 11:07:15 +0200

    todosSchema, "/todos" post

--- server.js
+++ server.js
@@ -6,12 +6,24 @@ const PORT = 4000;
 
 mongoose.connect("mongodb://localhost/todo-app-2");
 
-const userSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-})
+// User Schema
+    const userSchema = new mongoose.Schema({
+        username: String,
+        password: String,
+    })
+    const User = mongoose.model("User", userSchema)
 
-const User = mongoose.model("User", userSchema)
+// Todo tasks Schema
+    const todosSchema = new mongoose.Schema({
+        userId: String,
+        todos: [
+            {
+                checked: Boolean,
+                text: String,
+            }
+        ],
+    })
+    const Todos = mongoose.model("Todos", todosSchema)
 
 app.use(cors());
 app.use(express.json());
@@ -45,6 +57,42 @@ app.post("/login", async (req, res) => {
     res.json({
         message: "Success",
     });
+})
+
+app.post("/todos", async (req, res) => {
+    // We want to check which user accessed the "/todos" endpoint:
+        const { authorization } = req.headers; //saves the user credentials to this object
+        const [, token] = authorization.split(" ");
+        const [username, password] = token.split(":");
+        const todosItems = req.body;
+    
+    // If the user doesn't exist, we send an error message
+        const user = await User.findOne({ username }); //Checks if there is a username in db that's the same as the username passed in req
+        if(!user || user.password !== password){
+            res.status(403);
+            res.json({
+                message: "Invalid Login",
+            });
+            return;
+        }
+    
+    //Finds the todos Schema that matches the user's id
+        const todos = await Todos.findOne({userId: user._id}) 
+        
+        //If it won't find a todos Schema that match the user's id, it creates a new todos Schema for that user
+            if(!todos) {
+                await Todos.create({
+                    userId: user._id,
+                    todos: todosItems,
+                })
+            }
+        //If a todos Schema that matches the user's id already exists, it saves the todo task that was sent to the todos Schema
+            else{
+                todos.todos = todosItems;
+                await todos.save();
+            }
+    
+    res.json(todosItems);
 })
 
 
