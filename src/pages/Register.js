2c5347659547d75b1b3e4073274dd1ad5461975a
panosjapan7 <panosjapan@gmail.com>
2022-04-16 13:21:15 +0200

    Fixed typo

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -64,7 +64,7 @@ export function Register() {
             <button type="submit">Register</button>
         </form>
         <br />
-        <Link to="/login">Don't have an account? Login here.</Link>
+        <Link to="/login">Have an account? Login here.</Link>
     </div>
   )
 }
